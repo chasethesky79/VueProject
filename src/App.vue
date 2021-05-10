5835dabd3fe09bf7ece9d9b5cbf9a84f023fe80f
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-09 20:08:50 -0500

    Implemented validation for text field

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TwitterBoxClient />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TwitterBoxClient from "./components/TwitterBoxClient.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TwitterBoxClient,
   },
 };
 </script>
---
+++ src/components/TwitterBoxClient.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="input-form">
+    <h2>Project Twitter Box Client</h2>
+    <form @submit="submitForm" class="ui form">
+      <div class="form-input">
+        <input
+          v-model="newItem"
+          type="text"
+          placeholder="Enter text!"
+          @keydown="onKeyDown"
+        />
+        <button class="ui button">Submit</button>
+      </div>
+    </form>
+    <div v-if="items.length > 0" class="display-section">
+      <ul>
+        <li v-for="item in items" :key="item">
+          {{ item }},
+          <span style="margin-left: 10px"
+            >No of characters remaining: {{ 280 - item.length }}</span
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      newItem: "",
+      items: [],
+    };
+  },
+  methods: {
+    submitForm(event) {
+      this.items = [...this.items, this.newItem];
+      this.newItem = "";
+      event.preventDefault();
+    },
+    onKeyDown(event) {
+      if (this.newItem.length >= 280) {
+        event.preventDefault();
+        return;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+.form-input {
+  display: flex;
+  justify-content: center;
+}
+.form-input > * {
+  margin: 20px 20px 20px 20px;
+}
+.form-input input {
+  max-width: 500px;
+}
+.form-input {
+  margin: 20px 20px 20px 20px;
+}
+form,
+.display-section {
+  margin: auto;
+  width: 50%;
+  background-color: aquamarine;
+  border: 1px solid;
+}
+form {
+  margin-bottom: 30px;
+}
+</style>
